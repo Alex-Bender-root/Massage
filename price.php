9eed129f60efd9d9b5cd3a441c28534dd9a37282
Alex-Bender-root <produbl@rambler.ru>
2023-09-22 02:25:46 +0300

    s

--- price.php
+++ price.php
@@ -5,18 +5,285 @@ include('header.php');
 <body>
   <div class="page">
 
-    <div class="total">
 
 
+
+    <div class="av" id="av_prices">
+
+      <div class="av_foto">
+        <img src="img/prices/prices.webp">
+      </div>
+
+      <div class="av_desc">
+        <h1>Услуги и цены</h1>
+      </div>
 
 
     </div>
 
 
 
+    <div class="rp_full">
 
 
 
+
+      <div class="rp_content">
+
+        <div class="rp_con_desc">
+          <h2>Шейно-воротниковая зона</h2>
+          <p>Основная задача процедуры — снятие болезненных ощущений в районе затылка для облегчения состояния пациента. За счет улучшения притока крови и движения других полезных элементов к верхнему отделу позвоночника, значительно снижается риск возникновения необратимых дегенеративных процессов, которые рушат целостность костной ткани.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 20 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 300 руб</p>
+          </div>
+
+        </div>
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+
+      <div class="rp_content" id="rp_content_left">
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+        <div class="rp_con_desc" id="rp_con_desc_left">
+          <h2>Массаж лица</h2>
+          <p>Кожа лица требует наибольшего внимания и ухода. Она чрезвычайно чувствительна к внешним раздражителям и моментально откликается на них. На нее негативно влияют факторы внешнего окружения, стрессы и депрессии, малоподвижный образ жизни, плохая экология, недостаток сна и полноценного отдыха.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/dop.webp">
+            <p>Дополнительная слуга - маска + 150р</p>
+          </div>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 30 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 500 руб</p>
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+      <div class="rp_content">
+
+        <div class="rp_con_desc">
+          <h2>Шейно-воротниковая зона</h2>
+          <p>Основная задача процедуры — снятие болезненных ощущений в районе затылка для облегчения состояния пациента. За счет улучшения притока крови и движения других полезных элементов к верхнему отделу позвоночника, значительно снижается риск возникновения необратимых дегенеративных процессов, которые рушат целостность костной ткани.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 20 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 300 руб</p>
+          </div>
+
+        </div>
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+
+
+      <div class="rp_content" id="rp_content_left">
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+        <div class="rp_con_desc" id="rp_con_desc_left">
+          <h2>Шейно-воротниковая зона</h2>
+          <p>Основная задача процедуры — снятие болезненных ощущений в районе затылка для облегчения состояния пациента. За счет улучшения притока крови и движения других полезных элементов к верхнему отделу позвоночника, значительно снижается риск возникновения необратимых дегенеративных процессов, которые рушат целостность костной ткани.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 20 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 300 руб</p>
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+      <div class="rp_content">
+
+        <div class="rp_con_desc">
+          <h2>Шейно-воротниковая зона</h2>
+          <p>Основная задача процедуры — снятие болезненных ощущений в районе затылка для облегчения состояния пациента. За счет улучшения притока крови и движения других полезных элементов к верхнему отделу позвоночника, значительно снижается риск возникновения необратимых дегенеративных процессов, которые рушат целостность костной ткани.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 20 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 300 руб</p>
+          </div>
+
+        </div>
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+
+
+      <div class="rp_content" id="rp_content_left">
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+        <div class="rp_con_desc" id="rp_con_desc_left">
+          <h2>Шейно-воротниковая зона</h2>
+          <p>Основная задача процедуры — снятие болезненных ощущений в районе затылка для облегчения состояния пациента. За счет улучшения притока крови и движения других полезных элементов к верхнему отделу позвоночника, значительно снижается риск возникновения необратимых дегенеративных процессов, которые рушат целостность костной ткани.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 20 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 300 руб</p>
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+      <div class="rp_content">
+
+        <div class="rp_con_desc">
+          <h2>Шейно-воротниковая зона</h2>
+          <p>Основная задача процедуры — снятие болезненных ощущений в районе затылка для облегчения состояния пациента. За счет улучшения притока крови и движения других полезных элементов к верхнему отделу позвоночника, значительно снижается риск возникновения необратимых дегенеративных процессов, которые рушат целостность костной ткани.</p>
+          <span> </span>
+
+          <div class="rp_con_desc-b">
+            <img src="img/prices/time.webp">
+            <p>длительность 20 мин</p>
+          </div>
+
+          <div class="rp_con_desc-a">
+            <img src="img/prices/money.webp">
+            <p>стоимость 300 руб</p>
+          </div>
+
+        </div>
+
+        <div class="rp_con_f">
+          <img src="img/prices/1_lip.webp">
+          <div class="rp_con_f-a">
+            <img src="img/prices/content_prices.webp">
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+    </div>
 
 
 
